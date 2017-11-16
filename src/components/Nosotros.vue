9012e9c1e67f12f05fb197fb0ffab0be3d91d5bf
DESKTOP-M6V79DL\Mayra <mayrajuarez594@gmail.com>
2017-11-16 17:03:29 -0600

    vistas

---
+++ src/components/Nosotros.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container
+    fluid
+    grid-list-sm
+    tag="section"
+    id="grid">
+    <v-layout row wrap>
+      <v-flex tag="h1" class="headline">Nosotros</v-flex>
+      <v-flex d-flex xs12 order-xs5>
+        <v-layout column>
+          <v-flex>
+            <v-card flat>
+              <v-card-text
+                v-text="lorem">
+              </v-card-text>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card flat>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  export default {
+    data: () => ({
+      lorem: `Ponemos a tu alcance el servicio y administracion de localizacion de autobuses`
+    })
+  }
+</script>
